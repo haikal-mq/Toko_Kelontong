a2d039df80c92b54d80bd40a68abe6483a4b9272
Haikal Muzayyan Qudsi <haikalmuzayyan@gmail.com>
2026-06-10 23:38:47 +0700

    Update Project

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body class="p-4">
+    @include('navbar')
     <h1 class="text-2xl font-bold mb-5">Tabel Produk Toko Dua Bersaudara</h1>
     <button onclick="toggle_modal()" class="bg-blue-500 text-white px-4 py-2 rounded-2xl">+ Tambah Item</button>
     <table class="table-auto w-full mt-5">
